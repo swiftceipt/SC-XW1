10d007db93b3f599ff00d6b7fe861dbca2ff53ab
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-02-27 15:05:43 -0500

    added method that will handle login requests

--- routes/routes.js
+++ routes/routes.js
@@ -4,4 +4,13 @@ exports.init = function(app)
     {
         response.render("login_tester.ejs");
     });
+
+    app.post("/login", check_login);
+}
+
+check_login = function(request, response)
+{
+    console.log("\t" + request.body.email);
+    console.log("\t" + request.body.password);
+    response.render("login_tester.ejs");
 }
