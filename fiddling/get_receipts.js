04f9a30d66c1e82393d61d6c1cc6b96064fcf760
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-02-28 12:08:04 -0500

    added basic script that gets some receipts, still have to add correct time stamp

---
+++ fiddling/get_receipts.js
@@ -0,0 +1,60 @@
+var request = require('request');
+
+var options = {
+    url: "https://tenv-service.swiftceipt.com/signIn",
+    headers:
+    {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    },
+    json: true,
+    body: 
+    {
+        // these are the credientials for a dummy account
+        // that Kevin gave us
+        "email": "jack@cirno.de", 
+        "password": 1234
+    }
+};
+
+request.post(options,function (error, response, body)
+{
+    if(!error)
+    {
+        makeReceiptRequest(body.authToken);
+    }
+    else
+    {
+        console.log(error);
+    }
+});
+
+makeReceiptRequest = function(authToken)
+{
+    var options = {
+        url: "https://tenv-service.swiftceipt.com/getNewReceipts",
+        headers:
+        {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        json: true,
+        body: 
+        {
+            // use the token that we're provided
+            authToken: authToken
+        }
+    };
+
+    request.post(options, function(error, response, body)
+    {
+        if(!error)
+        {
+            console.log(body);
+        }
+        else
+        {
+            console.log(error);
+        }
+    });
+}
