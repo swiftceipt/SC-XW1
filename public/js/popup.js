4d611b1f8036605681c44ea42d1b8c5ed3300ca0
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-03-03 15:11:46 -0500

    added template to show original receipt

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,1 +1,9 @@
-console.log("hello");
+function popitup(url)
+{
+    var newwindow = window.open(url,'name','height = 200, width = 150');
+    if (window.focus)
+    {
+        newwindow.focus();
+    }
+    return false;
+}
