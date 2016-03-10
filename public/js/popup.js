c7b65de3ec5b5f7e82d1fe673e47cd449393233b
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-03-09 21:25:01 -0500

    removed popup related code, it wasn't going to be used

--- public/js/popup.js
+++
@@ -1,9 +0,0 @@
-function popitup(url)
-{
-    var newwindow = window.open(url,'name','height = 200, width = 150');
-    if (window.focus)
-    {
-        newwindow.focus();
-    }
-    return false;
-}
