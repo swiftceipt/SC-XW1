03aa21464aea3b4a0029fa4c7b46c4a31532c6c7
Eric Terui <eterui@andrew.cmu.edu>
2016-03-16 18:10:38 -0500

    formulated query string to use for Geocoding API

--- routes/map.js
+++ routes/map.js
@@ -1,8 +1,13 @@
+var request_api = require('request');
+
 /*
 Takes a receipt and adds lat and long to it
 */
 addLatLong = function(receipt)
 {
+	var query_string = receipt.address + " " + receipt.city + " " + receipt.state + " " + receipt.zipcode
+	console.log(query_string);
+
 	return receipt;
 }
 
