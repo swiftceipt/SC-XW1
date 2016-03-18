7dd43d35c65c59d4d1112ad4269f675fbabc0825
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-03-18 16:43:24 -0400

    successfully sent email for forgetful users

--- routes/forget.js
+++ routes/forget.js
@@ -1,16 +1,38 @@
 var validate = require("./validate");
+var request_api = require('request');
 
 forgetPath = function(request, response)
 {
     var content;
     if(validate.isEmail(request.body.email))
     {
-        response.render("login", {
-            message : {
-                type: "info",
-                content: "An reset email has been sent to \n " + request.body.email
+        var options = {
+            url: "https://tenv-service.swiftceipt.com/forgotPassword",
+            headers:
+            {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            json: true,
+            body: 
+            {
+                "email": request.body.email
+            }
+        }
+
+        request_api.post(options, function(error, api_response, body)
+        {
+            if(body.ackValue == "SUCCESS")
+            {
+                response.render("login", {
+                    message : {
+                        type: "info",
+                        content: "A reset email has been sent to \n " + request.body.email
+                    }
+                });
             }
         });
+
     }
     else
     {
