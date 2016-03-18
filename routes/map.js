37b2a4adda6fb44f83e394e8f6232ea246d723db
Eric Terui <eterui@andrew.cmu.edu>
2016-03-18 17:29:55 -0500

    trying request.get(query)

--- routes/map.js
+++ routes/map.js
@@ -5,8 +5,9 @@ Takes a receipt and adds lat and long to it
 */
 addLatLong = function(receipt)
 {
-	var query_string = receipt.address + " " + receipt.city + " " + receipt.state + " " + receipt.zipcode
+	var query_string = receipt.address + " " + receipt.city + " " + receipt.state + "&key=AIzaSyAplgHQOAr-awfM5ZsUm6X6a8aOgNH9W0Q";
 	console.log(query_string);
+	request_api.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + query_string)
 
 	return receipt;
 }
