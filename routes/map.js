684e4574d5c6e716a9df4a94910951fe2ac8de88
Eric Terui <eterui@andrew.cmu.edu>
2016-03-18 18:31:18 -0500

    receiving response from google maps geocoding api

--- routes/map.js
+++ routes/map.js
@@ -5,9 +5,27 @@ Takes a receipt and adds lat and long to it
 */
 addLatLong = function(receipt)
 {
-	var query_string = receipt.address + " " + receipt.city + " " + receipt.state + "&key=AIzaSyAplgHQOAr-awfM5ZsUm6X6a8aOgNH9W0Q";
-	console.log(query_string);
-	request_api.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + query_string)
+	var query_string = receipt.address + " " + receipt.city + " " + receipt.state;
+
+	// var options = {
+	// 	url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + query_string + "&key=AIzaSyAplgHQOAr-awfM5ZsUm6X6a8aOgNH9W0Q",
+	// 	headers:
+	// 	{
+ //            "Accept": "application/json",
+ //            "Content-Type": "application/json"
+ //        },
+ //        json: true
+	// }
+	// console.log(query_string);
+
+	// request_api.get(options, function (error, api_response, body)
+	// {
+	// 	console.log(body);
+	// });
+	
+	request_api("https://maps.googleapis.com/maps/api/geocode/json?address=" + query_string + "&key=AIzaSyAplgHQOAr-awfM5ZsUm6X6a8aOgNH9W0Q", function(error, response, body) {
+		console.log(body);
+	});
 
 	return receipt;
 }
