815fecb6a48e6dcbc56a9034b259413c5d0074f5
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-03-18 16:16:04 -0400

    validated email before checking

--- routes/forget.js
+++ routes/forget.js
@@ -1,9 +1,21 @@
+var validate = require("./validate");
+
 forgetPath = function(request, response)
 {
+    var content;
+    if(validate.isEmail(request.body.email))
+    {
+        content = "You seem to have forgotten your password\nWhy don't you check your email at: " + request.body.email
+    }
+    else
+    {
+        content = "That's not a valid email";
+    }
+
     response.render("login", {
         message : {
             type: "warning",
-            content: "You seem to have forgotten your password"
+            content: content
         }
     });
 }
