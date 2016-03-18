fab4a66530e57a892c4ddf3130753559044cddfe
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-03-18 16:24:58 -0400

    changed alert message type

--- routes/forget.js
+++ routes/forget.js
@@ -5,19 +5,23 @@ forgetPath = function(request, response)
     var content;
     if(validate.isEmail(request.body.email))
     {
-        content = "You seem to have forgotten your password\nWhy don't you check your email at: " + request.body.email
+        response.render("login", {
+            message : {
+                type: "info",
+                content: "An reset email has been sent to \n " + request.body.email
+            }
+        });
     }
     else
     {
-        content = "That's not a valid email";
+        response.render("login", {
+            message : {
+                type: "warning",
+                content: "That's not a valid email"
+            }
+        });
     }
 
-    response.render("login", {
-        message : {
-            type: "warning",
-            content: content
-        }
-    });
 }
 
 module.exports = {
