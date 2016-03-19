901fbd0f274cb4864e4b23de81cd65888b3cebb0
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-03-18 23:08:14 -0400

    took out commented out code and logged the geocoded geometry

--- routes/map.js
+++ routes/map.js
@@ -6,26 +6,12 @@ Takes a receipt and adds lat and long to it
 addLatLong = function(receipt)
 {
 	var query_string = receipt.address + " " + receipt.city + " " + receipt.state;
+	query_string = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query_string + "&key=AIzaSyAplgHQOAr-awfM5ZsUm6X6a8aOgNH9W0Q";
 
-	// var options = {
-	// 	url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + query_string + "&key=AIzaSyAplgHQOAr-awfM5ZsUm6X6a8aOgNH9W0Q",
-	// 	headers:
-	// 	{
- //            "Accept": "application/json",
- //            "Content-Type": "application/json"
- //        },
- //        json: true
-	// }
-	// console.log(query_string);
-
-	// request_api.get(options, function (error, api_response, body)
-	// {
-	// 	console.log(body);
-	// });
-	
-	request_api("https://maps.googleapis.com/maps/api/geocode/json?address=" + query_string + "&key=AIzaSyAplgHQOAr-awfM5ZsUm6X6a8aOgNH9W0Q", function(error, response, body) {
-		console.log(body);
-		// var jsonBody = JSON.parse(body);
+	request_api.get(query_string, function(error, response, body)
+	{
+		body = JSON.parse(body)
+		console.log(body.results[0].geometry.location);
 	});
 
 
