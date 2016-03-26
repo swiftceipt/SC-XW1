2843c3a9ed97e23af480fd10542b3efdaf468701
Ziyun Zheng <ziyunz@andrew.cmu.edu>
2016-03-26 12:36:10 -0400

    switch to plot.js and added functions to create graphs

--- public/js/script.js
+++ public/js/script.js
@@ -3,577 +3,67 @@ $(function() {
     $( "#tabs" ).tabs();
   });
 
+//fill the y axis data using the parsed data
+var month = [1,2,3,4,5,6,7,8,9,10,11,12]
+var value = []
+for (var i in by_month){
+    value.push(by_month[i])
+}
+var createDataYear = function(x,y,xtitle,ytitle,title,domElement){
+    var trace1 = {
+        x: x,
+        y: y,
+        type: 'scatter',
+        text: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug','Sep','Oct','Nov','Dec']
+        };
+    var data = [trace1];
+    var layout = {
+        title: title,
+        xaxis: {
+            title: xtitle
+        },
+        yaxis: {
+            title: ytitle
+        }
+    };
+    Plotly.newPlot(domElement, data,layout);
 
+}
 
-$(function () {
-    $('#month_graph').highcharts({
-        title: {
-            text: 'Monthly Average Temperature',
-            x: -20 //center
-        },
-        subtitle: {
-            text: 'Source: WorldClimate.com',
-            x: -20
-        },
-        xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        },
-        yAxis: {
-            title: {
-                text: 'Temperature (°C)'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        tooltip: {
-            valueSuffix: '°C'
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
-        },
-        series: [{
-            name: '2016',
-            data: [by_month['1'],by_month['2']
-            ,by_month['3'],by_month['4'],by_month['5'],
-            by_month['6'],by_month['7'],by_month['8'],
-            by_month['9'],by_month['10'],by_month['11'],by_month['12']]
-        }]
-    });
-});
-
-//january
-$(function () {
-    var stores = Object.keys(janurary);
-    var data = [];
-    for(i in stores){
-        console.log(i);
-        store = {};
-        store['name'] = stores[i];
-        store['data'] = [janurary[stores[i]]];
-        data.push(store);
+var createAxisData = function(data){
+    var xaxis = Object.keys(data)
+    var yaxis = []
+    for (var i in data){
+        yaxis.push(data[i])
     }
-    console.log(data);
-    $('#janurary_graph').highcharts({
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Column chart with negative values'
-        },
-        xAxis: {
-            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-        },
-        credits: {
-            enabled: false
-        },
-        series: data    });
-});
+    return {"xaxis":xaxis,"yaxis":yaxis}
+}
 
-//february
-$(function () {
-    var stores = Object.keys(february);
-    var data = [];
-    for(i in stores){
-        console.log(i);
-        store = {};
-        store['name'] = stores[i];
-        store['data'] = [february[stores[i]]];
-        data.push(store);
+var createDataMonth = function(x, y, xtitle, ytitle,title,domElement){
+    var data = [
+    {
+        x: x,
+        y: y,
+        text: x,
+        type: 'bar'
     }
-    console.log(data);
-    $('#february_graph').highcharts({
-        chart: {
-            type: 'column'
+    ];
+    layout = {
+        title: title,
+        xaxis: {
+            title: xtitle
         },
-        title: {
-            text: 'Column chart with negative values'
-        },
-        xAxis: {
-            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-        },
-        credits: {
-            enabled: false
-        },
-        series: data    });
-});
+        yaxis: {
+            title: ytitle
+        }
+    };
+    Plotly.newPlot(domElement, data,layout);
+}
+
+//create monthly graph
+createDataYear(month,value,"Month","Spendings in dollars","Monthly Spendings",'monthly');
+//create January Graph
+axis = createAxisData(janurary);
+createDataMonth(axis["xaxis"],axis["yaxis"],"Stores","Spendings","Janurary's Spendings",'jan')
 
 
-//march
-$(function () {
-    var stores = Object.keys(march);
-    var data = [];
-    for(i in stores){
-        console.log(i);
-        store = {};
-        store['name'] = stores[i];
-        store['data'] = [march[stores[i]]];
-        data.push(store);
-    }
-    console.log(data);
-    $('#march_graph').highcharts({
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Column chart with negative values'
-        },
-        xAxis: {
-            categories: ['Money', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-        },
-        credits: {
-            enabled: false
-        },
-        series: data    });
-});
-
-
-
-
-//april
-$(function () {
-    var stores = Object.keys(april);
-    var data = [];
-    for(i in stores){
-        console.log(i);
-        store = {};
-        store['name'] = stores[i];
-        store['data'] = [april[stores[i]]];
-        data.push(store);
-    }
-    console.log(data);
-    $('#april_graph').highcharts({
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Column chart with negative values'
-        },
-        xAxis: {
-            categories: ['Money', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-        },
-        credits: {
-            enabled: false
-        },
-        series: data    });
-});
-
-
-
-
-
-
-//may
-$(function () {
-    var stores = Object.keys(may);
-    var data = [];
-    for(i in stores){
-        console.log(i);
-        store = {};
-        store['name'] = stores[i];
-        store['y'] = april[stores[i]];
-        data.push(store);
-    }
-    console.log(data);
-
-    $(document).ready(function () {
-
-        // Build the chart
-        $('#may_graph').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Monthly Spending, 2016 April'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: data
-            }]
-        });
-    });
-});
-
-
-
-
-
-//june
-$(function () {
-    var stores = Object.keys(june);
-    var data = [];
-    for(i in stores){
-        console.log(i);
-        store = {};
-        store['name'] = stores[i];
-        store['y'] = april[stores[i]];
-        data.push(store);
-    }
-    console.log(data);
-
-    $(document).ready(function () {
-
-        // Build the chart
-        $('#june_graph').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Monthly Spending, 2016 April'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: data
-            }]
-        });
-    });
-});
-
-
-
-
-
-//july
-$(function () {
-    var stores = Object.keys(july);
-    var data = [];
-    for(i in stores){
-        console.log(i);
-        store = {};
-        store['name'] = stores[i];
-        store['y'] = april[stores[i]];
-        data.push(store);
-    }
-    console.log(data);
-
-    $(document).ready(function () {
-
-        // Build the chart
-        $('#july_graph').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Monthly Spending, 2016 April'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: data
-            }]
-        });
-    });
-});
-
-
-
-
-
-//august
-$(function () {
-    var stores = Object.keys(august);
-    var data = [];
-    for(i in stores){
-        console.log(i);
-        store = {};
-        store['name'] = stores[i];
-        store['y'] = april[stores[i]];
-        data.push(store);
-    }
-    console.log(data);
-
-    $(document).ready(function () {
-
-        // Build the chart
-        $('#august_graph').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Monthly Spending, 2016 April'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: data
-            }]
-        });
-    });
-});
-
-
-
-
-//sept
-$(function () {
-    var stores = Object.keys(step);
-    var data = [];
-    for(i in stores){
-        console.log(i);
-        store = {};
-        store['name'] = stores[i];
-        store['y'] = april[stores[i]];
-        data.push(store);
-    }
-    console.log(data);
-
-    $(document).ready(function () {
-
-        // Build the chart
-        $('#sept_graph').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Monthly Spending, 2016 April'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: data
-            }]
-        });
-    });
-});
-
-
-
-
-
-//oct
-$(function () {
-    var stores = Object.keys(oct);
-    var data = [];
-    for(i in stores){
-        console.log(i);
-        store = {};
-        store['name'] = stores[i];
-        store['y'] = april[stores[i]];
-        data.push(store);
-    }
-    console.log(data);
-
-    $(document).ready(function () {
-
-        // Build the chart
-        $('#oct_graph').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Monthly Spending, 2016 April'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: data
-            }]
-        });
-    });
-});
-
-
-
-
-//nov
-$(function () {
-    var stores = Object.keys(nov);
-    var data = [];
-    for(i in stores){
-        console.log(i);
-        store = {};
-        store['name'] = stores[i];
-        store['y'] = april[stores[i]];
-        data.push(store);
-    }
-    console.log(data);
-
-    $(document).ready(function () {
-
-        // Build the chart
-        $('#nov_graph').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Monthly Spending, 2016 April'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: data
-            }]
-        });
-    });
-});
-
-
-
-
-//dec
-$(function () {
-    var stores = Object.keys(dec);
-    var data = [];
-    for(i in stores){
-        console.log(i);
-        store = {};
-        store['name'] = stores[i];
-        store['y'] = april[stores[i]];
-        data.push(store);
-    }
-    console.log(data);
-
-    $(document).ready(function () {
-
-        // Build the chart
-        $('#dec_graph').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Monthly Spending, 2016 April'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: data
-            }]
-        });
-    });
-});
