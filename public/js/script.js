be99b57eda6e61cf78704523225934306afa1b1c
Ziyun Zheng <ziyunz@andrew.cmu.edu>
2016-03-26 14:08:35 -0400

    funtion works and tested

--- public/js/script.js
+++ public/js/script.js
@@ -39,31 +39,88 @@ var createAxisData = function(data){
     return {"xaxis":xaxis,"yaxis":yaxis}
 }
 
-var createDataMonth = function(x, y, xtitle, ytitle,title,domElement){
-    var data = [
-    {
-        x: x,
-        y: y,
-        text: x,
-        type: 'bar'
+
+var createDataMonth = function(x, y, xtitle, ytitle,title,domElement,domMsgElement){
+    if (x.length==0 && y.length==0){
+        //display message saying there is no spendings for this month
+        console.log("empty");
+         document.getElementById(domMsgElement).innerHTML = "You did not spend any money this month";
     }
-    ];
-    layout = {
-        title: title,
-        xaxis: {
-            title: xtitle
-        },
-        yaxis: {
-            title: ytitle
+    else{
+        var data = [
+        {
+            x: x,
+            y: y,
+            text: x,
+            type: 'bar'
         }
-    };
-    Plotly.newPlot(domElement, data,layout);
+        ];
+        layout = {
+            title: title,
+            xaxis: {
+                title: xtitle
+            },
+            yaxis: {
+                title: ytitle
+            }
+        };
+        Plotly.newPlot(domElement, data,layout);
+    }
 }
 
 //create monthly graph
 createDataYear(month,value,"Month","Spendings in dollars","Monthly Spendings",'monthly');
+
+
 //create January Graph
-axis = createAxisData(janurary);
-createDataMonth(axis["xaxis"],axis["yaxis"],"Stores","Spendings","Janurary's Spendings",'jan')
+axis1 = createAxisData(janurary);
+createDataMonth(axis1["xaxis"],axis1["yaxis"],
+    "Stores","Spendings","Janurary's Spendings",'jan','jan-message')
+//create February Graph
+axis2 = createAxisData(february);
+createDataMonth(axis2["xaxis"],axis2["yaxis"],
+    "Stores","Spendings","February's Spendings",'feb','feb-message')
+//create March Graph
+axis3 = createAxisData(march);
+createDataMonth(axis3["xaxis"],axis3["yaxis"],
+    "Stores","Spendings","March's Spendings",'mar','mar-message')
+//create April Graph
+axis4 = createAxisData(april);
+createDataMonth(axis4["xaxis"],axis4["yaxis"],
+    "Stores","Spendings","April's Spendings",'apr','apr-message')
+//create May Graph
+axis5 = createAxisData(may);
+createDataMonth(axis5["xaxis"],axis5["yaxis"],
+    "Stores","Spendings","May's Spendings",'may','may-message')
+//Create June Graph
+axis6 = createAxisData(june);
+createDataMonth(axis6["xaxis"],axis6["yaxis"],
+    "Stores","Spendings","June's Spendings",'jun','jun-message')
+//create July Graph
+axis7 = createAxisData(july);
+createDataMonth(axis7["xaxis"],axis7["yaxis"],
+    "Stores","Spendings","July's Spendings",'jul','jul-message')
+//create August graph
+axis8 = createAxisData(august);
+createDataMonth(axis8["xaxis"],axis8["yaxis"],
+    "Stores","Spendings","August's Spendings",'aug','aug-message')
+//create september graph
+axis9 = createAxisData(september);
+createDataMonth(axis9["xaxis"],axis9["yaxis"],
+    "Stores","Spendings","September's Spendings",'sep','sep-message')
+//create october graph
+axis10 = createAxisData(october);
+createDataMonth(axis10["xaxis"],axis10["yaxis"],
+    "Stores","Spendings","October's Spendings",'oct','oct-message')
+//create november graph
+axis11 = createAxisData(november);
+createDataMonth(axis11["xaxis"],axis11["yaxis"],
+    "Stores","Spendings","November's Spendings",'nov','nov-message')
+//create december graph
+axis12 = createAxisData(december);
+createDataMonth(axis12["xaxis"],axis12["yaxis"],
+    "Stores","Spendings","December's Spendings",'dec','dec-message')
+
+
 
 
