770116802b02f2a969242777f7ce2ce9877f02f3
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-03-26 22:32:14 -0400

    dynamically generated change in response area

--- public/js/receipts_animation.js
+++ public/js/receipts_animation.js
@@ -2,12 +2,13 @@ window.onload = function()
 {
     // apply a click handler to each list group item
     var list_group_items = document.getElementsByClassName("list-group-item");
+    var responseArea = document.getElementById("receiptArea");
 
     for(var i = 0; i < list_group_items.length; i ++)
     {
         list_group_items[i].onclick = function()
         {
-            alert("show me the money");
+            responseArea.innerHTML = this.innerHTML;
         }
     }
 };
