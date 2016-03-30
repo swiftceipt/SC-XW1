caccef94052a6924972e2092d727e76140c11902
Ziyun Zheng <ziyunz@andrew.cmu.edu>
2016-03-30 15:16:38 -0400

    created droppable functions and styled the hover state and draggable element

--- public/js/drag_and_drop.js
+++ public/js/drag_and_drop.js
@@ -6,6 +6,18 @@ function init() {
   	zIndex: 999999999,
   	stack: "#container",
   	cursor: "move", 
-  	cursorAt: { top: -5, left: -5 }
+  	opacity: 0.65,
+  	cursorAt: { top: 30, left: 20 },
+  	//stop:handleDragStop
   });
+  $('.folder-group-item').droppable( {
+    drop: handleDropStop,
+    accept: ".list-group-item",
+    hoverClass: "ui-state-hover",
+  } );
+}
+function handleDropStop( event, ui ) {
+  var element_id = ui.draggable.attr("id");
+  var element_drop_id = event.target.id;
+  alert( "you have dragged"+ " "+ element_id + " on to " +element_drop_id );
 }
