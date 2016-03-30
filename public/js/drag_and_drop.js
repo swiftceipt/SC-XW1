b6d9124af5a806769843731c74f956bda9ac59f0
Ziyun Zheng <ziyunz@andrew.cmu.edu>
2016-03-30 12:13:53 -0400

    fixed the hiding behind side bar issue

--- public/js/drag_and_drop.js
+++ public/js/drag_and_drop.js
@@ -3,7 +3,7 @@ $( init );
 function init() {
   $('.list-group-item').draggable({
   	helper: "clone",
-  	zIndex: 100,
+  	zIndex: 999999999,
   	stack: "#container",
   	cursor: "move", 
   	cursorAt: { top: -5, left: -5 }
