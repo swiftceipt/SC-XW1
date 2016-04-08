1e0af55c9578ae3562752c3b6f5f7787fc970139
Eric Terui <eterui@andrew.cmu.edu>
2016-04-08 15:37:20 -0400

    create folder button properly turns into text box form. Also, the button conforms to our design now

--- public/js/folders.js
+++ public/js/folders.js
@@ -2,3 +2,8 @@ function renameForm(i, name) {
     // turns the folder li into a text box to rename the folder
     $("#folder_" + i).replaceWith("<form action='/rename_folder/" + name + "' method='post'><input type='text' name='newFolderName' placeholder='" + name + "'></form>")
 }
+
+function nameFolder() {
+	// turns the 'create folder' button into a text box
+	$("#nameFolder").replaceWith("<form action='/create_folder' method='post'><input type='text' name='folderName' placeholder='Folder Name'></form>")
+}
