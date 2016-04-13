730d7289ca3dd4c7e7738d730b095c8a1bf20758
Eric Terui <eterui@andrew.cmu.edu>
2016-04-13 16:04:27 -0400

    applied validation for folder name on rename form

--- public/js/folders.js
+++ public/js/folders.js
@@ -3,37 +3,44 @@ function renameForm(i, name) {
     var renameForm = $("<form><input type='text' name='newFolderName' placeholder='" + name + "'></form>");
     var selector = "[id='" + name + "']"
     $(selector).replaceWith(renameForm);
-    renameForm.submit(function()
-    {
-        $.ajax({
-            type: "POST",
-            url: "/rename_folder/" + name,
-            contentType: "application/json",
-            dataType: "json",
-            data: JSON.stringify({
-                newFolderName: renameForm.find("input").val()
-            }),
-            success: function(data, status)
-            {
-                //window.reload();
-                window.location.href = '/receipts';
-                toastr.success( "Renamed folder successfully");
-            },
-            error: function(xhr, status, message)
-            {
-                console.log("frontend error");
-                console.log(xhr.responseJSON.status);
-                if(xhr.responseJSON.status == 'already exists')
+    renameForm.submit(function() {
+        var folderName = renameForm.find("input").val();
+        if (!isValidFolderName(folderName))
+        {
+            toastr["warning"]("Please choose a folder name that contains only letters, numbers, and is less than 40 characters long.")
+        }
+        else
+        {
+            $.ajax({
+                type: "POST",
+                url: "/rename_folder/" + name,
+                contentType: "application/json",
+                dataType: "json",
+                data: JSON.stringify({
+                    newFolderName: folderName
+                }),
+                success: function(data, status)
                 {
-                    toastr["warning"]("A folder with this name already exists! Please choose a new name.");
-                }
-                else
+                    //window.reload();
+                    window.location.href = '/receipts';
+                    toastr.success( "Renamed folder successfully");
+                },
+                error: function(xhr, status, message)
                 {
-                    toastr["error"]( "Sorry, something went wrong.");
+                    console.log("frontend error");
+                    console.log(xhr.responseJSON.status);
+                    if(xhr.responseJSON.status == 'already exists')
+                    {
+                        toastr["warning"]("A folder with this name already exists! Please choose a new name.");
+                    }
+                    else
+                    {
+                        toastr["error"]( "Sorry, something went wrong.");
+                    }
+                      
                 }
-                  
-            }
-        });
+            });
+        }
         return false;
     } )
 }
@@ -45,7 +52,6 @@ function nameFolder() {
     $("#nameFolder").replaceWith(nameForm);
     nameForm.submit(function() {
         folderName = nameForm.find("input").val();
-        console.log(nameForm.find("input").val());
         if (!isValidFolderName(folderName))
         {
             toastr["warning"]("Please choose a folder name that contains only letters, numbers, and is less than 40 characters long.")
