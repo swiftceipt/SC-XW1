c9b655121bb0be7c2d63ea962543b0665c849b32
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-04-17 16:33:45 -0400

    added more validation tests

--- test/test-example.js
+++ test/test-example.js
@@ -1,24 +1,48 @@
 var assert = require('chai').assert;
 var validate_module = require('../routes/validate');
 
-describe('Validate', function()
+describe('isEmail(str)', function()
 {
-    describe('isEmail(str)', function ()
+    it('should allow valid emails', function()
     {
-        it('should allow valid emails', function()
-        {
-            assert.isTrue(validate_module.isEmail("jack@cirno.de"));
-            assert.isTrue(validate_module.isEmail("skmehta@andrew.cmu.edu"));
-            assert.isTrue(validate_module.isEmail("kevin@swiftceipt.com"));
-            assert.isTrue(validate_module.isEmail("joemertz@cmu.edu"));
-        });
-
-        it('should not allow invalid emails', function()
-        {
-            assert.isFalse(validate_module.isEmail("@cirno.de"));
-            assert.isFalse(validate_module.isEmail("jack@.de"));
-            assert.isFalse(validate_module.isEmail("jack@cirno"));
-            assert.isFalse(validate_module.isEmail("!@%$$@cirno.de"));
-        });
+        assert.isTrue(validate_module.isEmail("jack@cirno.de"));
+        assert.isTrue(validate_module.isEmail("skmehta@andrew.cmu.edu"));
+        assert.isTrue(validate_module.isEmail("kevin@swiftceipt.com"));
+        assert.isTrue(validate_module.isEmail("joemertz@cmu.edu"));
     });
+
+    it('should not allow invalid emails', function()
+    {
+        assert.isFalse(validate_module.isEmail("@cirno.de"));
+        assert.isFalse(validate_module.isEmail("jack@.de"));
+        assert.isFalse(validate_module.isEmail("jack@cirno"));
+        assert.isFalse(validate_module.isEmail("!@%$$@cirno.de"));
+        assert.isFalse(validate_module.isEmail("lasdkjfhlajfhd"));
+    });
+});
+
+describe("new_user(body)", function()
+{
+    var body = {}
+
+    body.email = undefined;
+    assert.isNotTrue(validate_module.new_user(body));
+    body.email = "";
+    assert.isNotTrue(validate_module.new_user(body));
+    body.email = "laskdjfhladskjhfkljashdflkjahslkjdfhalkjsdfhlkajshdflkjahsflkdjhaslkjdfhlaksjhdflkajhsdflkjhasldkjfhalksdjhflakjsdhflkjhasldkfjhalkjfhd";
+    assert.isNotTrue(validate_module.new_user(body));
+    body.email = ")(*&)";
+    assert.isNotTrue(validate_module.new_user(body));
+    body.email = "jack@cirno.de"
+
+    body.username = undefined;
+    assert.isNotTrue(validate_module.new_user(body));
+    body.username = "";
+    assert.isNotTrue(validate_module.new_user(body));
+    body.username = "ladskjfhlkasjhdflkajhsldfkjhaslkjdfhalksjhdflkajhsflkjah";
+    assert.isNotTrue(validate_module.new_user(body));
+    body.username = "!@$@#!";
+    assert.isNotTrue(validate_module.new_user(body));
+
+
 });
