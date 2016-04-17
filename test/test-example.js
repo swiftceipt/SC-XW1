e98babaaabe25f3bcf587dcaff2a7f88875558b7
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-04-17 16:16:35 -0400

    added invalid email test

--- test/test-example.js
+++ test/test-example.js
@@ -3,14 +3,22 @@ var validate_module = require('../routes/validate');
 
 describe('Validate', function()
 {
-  describe('isEmail(str)', function ()
-  {
-    it('should allow valid emails', function ()
+    describe('isEmail(str)', function ()
     {
-      assert.isTrue(validate_module.isEmail("jack@cirno.de"));
-      assert.isTrue(validate_module.isEmail("skmehta@andrew.cmu.edu"));
-      assert.isTrue(validate_module.isEmail("kevin@swiftceipt.com"));
-      assert.isTrue(validate_module.isEmail("joemertz@cmu.edu"));
+        it('should allow valid emails', function()
+        {
+            assert.isTrue(validate_module.isEmail("jack@cirno.de"));
+            assert.isTrue(validate_module.isEmail("skmehta@andrew.cmu.edu"));
+            assert.isTrue(validate_module.isEmail("kevin@swiftceipt.com"));
+            assert.isTrue(validate_module.isEmail("joemertz@cmu.edu"));
+        });
+
+        it('should not allow invalid emails', function()
+        {
+            assert.isFalse(validate_module.isEmail("@cirno.de"));
+            assert.isFalse(validate_module.isEmail("jack@.de"));
+            assert.isFalse(validate_module.isEmail("jack@cirno"));
+            assert.isFalse(validate_module.isEmail("!@%$$@cirno.de"));
+        });
     });
-  });
 });
