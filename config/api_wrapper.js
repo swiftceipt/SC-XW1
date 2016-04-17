04888a62df7fe585f5729dcc24f0070264e900e5
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-04-17 18:30:10 -0400

    now can use all method calls, despite SC API being all POST right now

--- config/api_wrapper.js
+++ config/api_wrapper.js
@@ -2,7 +2,7 @@ var request = require('request');
 var config = require('./config.json');
 
 
-make_api_call = function(endpoint, body, callback)
+make_api_call = function(endpoint, body, callback, method)
 {
     var options = {
         url: config.api_endpoint + endpoint,
@@ -15,7 +15,8 @@ make_api_call = function(endpoint, body, callback)
         body: body
     };
 
-    request.post(options, callback);
+    method = method || "post";
+    request[method](options, callback);
 }
 
 module.exports = {
