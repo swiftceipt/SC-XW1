9095737060eee81d3fc27f29b6ac9b040a5f651e
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-04-22 17:11:54 -0400

    added login at beginning and logout before each test

--- test/test-folders.js
+++ test/test-folders.js
@@ -18,7 +18,37 @@ describe("Folder Functionality", function()
     this.timeout(0);
     var url = "https://" + config.server.ipaddress + ":" + config.server.port;
 
-    it("should display the folders in the sidebar");
+    before(function()
+    {
+        browser.visit(url + "/logout", function(done)
+        {
+            contains("ul.nav", "Register");
+        });
+    });
+
+    it("should successfully log in", function(done)
+    {
+        assert.isTrue(typeof browser != "undefined");
+        assert.isTrue(browser instanceof Browser);
+
+        browser.visit(url + "/login").then(function()
+        {
+            assert.isTrue(browser.success);
+            assert.isTrue(browser.query("button[type='submit']").innerHTML == "Sign in");
+        }).then(done, function(done2)
+        {
+
+            browser
+            .fill('input[name="email"]', "jack@cirno.de")
+            .fill('input[name="password"]', "1234")
+            .pressButton('button[type="submit"]', function()
+            {
+                assert.isTrue(contains(".jumbotron", "get started"));
+                done2();
+            });
+        });
+    });
+
     it("if there is no 'food' folder defined, should be able to create it");
     it("should be able to change the folder name to 'delicious food'");
     it("should be able to delete the folder");
